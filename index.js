a37e563290c0ba2e7780265b2ba660c34bc155e2
Sadhix <moizhussain89569@gmail.com>
2026-09-24 21:22:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins-deployed Node app!');
+  res.send('Hello world');
 });
 
 app.listen(PORT, () => {
